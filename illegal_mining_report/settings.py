36638db5c98d6cdc65d96f61b13b55adee24c5d7
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2024-10-07 20:11:16 +0000

    setting up vercel

--- illegal_mining_report/settings.py
+++ illegal_mining_report/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-n9)epi&+mqhr3q82!ag38#!6$5=zw8+wc23&u@8$!jw2^cb7&=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
