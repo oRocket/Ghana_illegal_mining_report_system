acadb3748b468ff10a97114e0cae965be68d3276
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2024-10-12 10:10:46 +0000

    build_files fixed

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,7 @@
-# build_files.sh
+#!/bin/bash
 
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+# Install dependencies using full path to pip
+C:/Users/Albert/Documents/DJANGO\ PROJECT/SOCIAL\ JUSTICE\ HACKATHON/venv/Scripts/pip.exe install -r requirements.txt
+
+# Collect static files for Django
+python manage.py collectstatic --noinput
